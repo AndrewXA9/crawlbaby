02aa090ae7f23b8c1eccf9646de6846bd6f546c2
andrewxa9 <andrewxa9@yahoo.com>
2015-01-20 22:01:58 -0800

    camera & movement plus dragon

---
+++ Assets/CamCtrl.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamCtrl : MonoBehaviour {
+	
+	public float backDist;
+	public float upDist;
+	public float focusHeight;
+	public float snap;
+	public float verticalSpeed;
+	
+	public float minAngle;
+	public float maxAngle;
+	
+	public float transparentDist;
+	
+	private float angle;
+	
+	private GameObject player;
+	private Vector3 target;
+	
+	
+	
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		//Screen.lockCursor = true;
+	}
+	
+	void Update () {
+		
+		
+		angle += Input.GetAxis("Mouse Y")*Time.deltaTime*verticalSpeed;
+		angle = Mathf.Clamp(angle,minAngle,maxAngle);
+		
+		this.transform.position = Vector3.Lerp(this.transform.position,target,snap*Time.deltaTime);
+		player.transform.position+=(Vector3.up*focusHeight);
+		this.transform.LookAt(player.transform.position);
+		player.transform.position-=(Vector3.up*focusHeight);
+		
+		target = ((player.transform.rotation*Quaternion.Euler(angle,0f,0f))*((Vector3.back*backDist)+(Vector3.up*upDist)))+player.transform.position;
+		
+		if(Input.GetMouseButtonDown(0)){
+			Screen.lockCursor = true;
+		}
+		
+		/*RaycastHit ray;
+		if(Physics.Raycast(this.transform.position,this.transform.forward,out ray,transparentDist)){
+			ray.collider.renderer.material.color = Color.red;
+		}*/
+		
+	}
+	
+	void OnDrawGizmos(){
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(target,.25f);
+	}
+	
+}
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,13 +3,88 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public GameObject up;
+	public GameObject down;
+	private CapsuleCollider colliderUp;
+	private SphereCollider colliderDown;
 	
+	private GameObject climbPoint;
+	
+	public enum states {walk,crawl,run,jump,climb,grab};
+	public states state = states.walk;
+	
+	public float walkspeed;
+	public float crawlspeed;
+	public float runspeed;
+	
+	public float turnspeed;
+	
+	private float speed;
+	
+	void Start(){
+		climbPoint = GameObject.Find("Climb");
+		
+		speed = walkspeed;
+		
+		colliderUp = this.gameObject.GetComponent<CapsuleCollider>();
+		colliderDown = this.gameObject.GetComponent<SphereCollider>();
+		colliderDown.enabled = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update(){
+		
+		
+		//BASIC MOVEEMTN
+		if(state == states.walk || state == states.run || state == states.crawl){
+			Vector3 hMove = this.transform.right*Input.GetAxis("Horizontal")*(speed*Time.deltaTime)/2;
+			Vector3 vMove = this.transform.forward*Input.GetAxis("Vertical")*(speed*Time.deltaTime);
+			
+			if(!Physics.Raycast(new Ray(this.transform.position+(Vector3.up*0.2f)+(hMove.normalized*(colliderDown.radius)),hMove.normalized),hMove.magnitude)){
+				this.transform.position += hMove;
+			}
+			if(!Physics.Raycast(new Ray(this.transform.position+(Vector3.up*0.2f)+(vMove.normalized*(colliderDown.radius)),vMove.normalized),vMove.magnitude)){
+				this.transform.position += vMove;
+			}
+		}
+		//ROTATE
+		this.transform.RotateAround(this.transform.position,Vector3.up,Input.GetAxis("Mouse X")*Time.deltaTime*turnspeed);
+			
+		//CRAWL
+		if(Input.GetKeyDown(KeyCode.LeftControl)){
+			if(state == states.walk || state == states.run){
+				state = states.crawl;
+				up.SetActive(false);
+				down.SetActive(true);
+				colliderUp.enabled = false;
+				colliderDown.enabled = true; 
+				speed = crawlspeed;
+			}
+		}
+		if(Input.GetKeyUp(KeyCode.LeftControl)){
+			state = states.walk;
+			up.SetActive(true);
+			down.SetActive(false);
+			colliderUp.enabled = true;
+			colliderDown.enabled = false;
+			speed = walkspeed;
+		}
+		
+		
+		
+		
 	}
+	
+	void OnDrawGizmos(){
+		if(Time.time > 0){
+			Vector3 hMove = this.transform.right*Input.GetAxis("Horizontal")*(speed*Time.deltaTime)/2;
+			Vector3 vMove = this.transform.forward*Input.GetAxis("Vertical")*(speed*Time.deltaTime);
+			
+			Gizmos.color = Color.red;
+			Gizmos.DrawSphere(this.transform.position+(Vector3.up*0.2f),0.05f);
+			Gizmos.DrawRay(this.transform.position+(Vector3.up*0.2f)+(hMove.normalized*(colliderDown.radius)),hMove);
+			Gizmos.DrawRay(this.transform.position+(Vector3.up*0.2f)+(vMove.normalized*(colliderDown.radius)),vMove);
+		}
+	}
+	
 }
