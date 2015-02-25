520a06c773a5ef75100e1d0b596158d055294bb9
andrewxa9 <andrewxa9@yahoo.com>
2015-02-24 21:55:53 -0800

    alerts, guard finished, removed image effects

---
+++ Assets/Guard/Pathing.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pathing : MonoBehaviour {
+
+	private List<Transform> nodes;
+	private bool move = false;
+	public  float waitTime;
+	private float currTime = 0;
+	public float moveSpeed;
+	private int target = 0;
+	private int maxTarget;
+	
+	void Start () {
+		nodes = new List<Transform>();
+		for(int i=0;i<this.transform.childCount;i++){
+			if(this.transform.GetChild(i).name == "MoveNode"){
+				nodes.Add(this.transform.GetChild(i));
+				this.transform.GetChild(i).gameObject.renderer.enabled = false;
+			}
+		}
+		foreach(Transform i in nodes){
+			i.parent = null;
+		}
+	}
+	
+	void Update(){
+		if(move){
+			if((this.transform.position-nodes[target].position).magnitude > moveSpeed){
+				this.transform.position += (nodes[target].position-this.transform.position).normalized*moveSpeed;
+			}
+			else{
+				target = (target+1)%(nodes.Count);
+				move = false;
+			}
+		}
+		else{
+			if(currTime < waitTime){
+				currTime += Time.deltaTime;
+				//Debug.Log(currTime);
+			}
+			else{
+				currTime = 0;
+				move = true;
+				this.transform.LookAt(nodes[target].position);
+			}
+			//Debug.Log(move);
+		}
+	}
+	
+	void OnDrawGizmos(){
+		if(Time.time > 0){
+			for(int i=0;i<nodes.Count;i++){
+				Gizmos.color = Color.green;
+				Gizmos.DrawLine(nodes[i].position,nodes[(i+1)%nodes.Count].position);
+				Gizmos.color = Color.yellow;
+				int c = 4;
+				for(int j=0;j<c;j++){
+					Gizmos.DrawSphere(Vector3.Lerp(nodes[i].position,nodes[(i+1)%nodes.Count].position,((Time.time/2f)+((1.0f/(float)c)*(float)j))%1),0.2f);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Player/GetSeen.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetSeen : MonoBehaviour {
+	
+	public GameObject alert;
+	public bool alertOn = false;
+	
+	private AudioSource audio;
+	
+	public float TimeLimit;
+	private float currTime = 0;
+	private float resetDelay = 0;
+	
+	private int state = 0;
+	
+	private int alerts = 0;
+	
+	public void Start(){
+		audio = this.gameObject.GetComponent<AudioSource>();
+	}
+	
+	public void Update(){
+		if(resetDelay > 0f){
+			resetDelay -= Time.deltaTime;
+		}
+		else{
+			if(currTime > 0f){
+				currTime -= Time.deltaTime;
+			}
+		}
+		
+		if(alerts == 0 && alertOn){
+			alert.SetActive(false);
+			alertOn = false;
+		}
+		
+		alerts = 0;
+	}
+	
+	public void Alert(){
+		if(alerts == 0){
+			currTime += Time.deltaTime;
+			currTime = Mathf.Clamp(currTime,0,TimeLimit);
+			if(resetDelay <= 0){
+				alert.SetActive(true);
+				alertOn = true;
+				audio.Play();
+			}
+			resetDelay = 1f;
+		}
+		alerts++;
+	}
+	
+	public void OnGUI(){
+		if(currTime > 0){
+			GUI.Box(new Rect(0,0,Screen.width*(currTime/TimeLimit),100),"lolololooololol");
+		}
+		GUI.Box(new Rect(0,100,50,50),alerts.ToString());
+	}
+	
+}
